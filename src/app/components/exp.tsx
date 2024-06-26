8bd6d0702686e25b660cc29609f8e8120a0e525b
Monishobaid <monishobaid@gmail.com>
2024-06-27 03:16:50 +0530

    changes

--- src/app/components/exp.tsx
+++ src/app/components/exp.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 
 interface ExperienceItemProps {
   title: string;
-  company: string;
-  location: string;
-  date: string;
-  points: string[];
+  company: string,
+  location: string,
+  date: string,
+  points: string[]
 }
 
 const ExperienceItem: React.FC<ExperienceItemProps> = ({
@@ -14,34 +15,72 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({
   location,
   date,
   points,
-}) => (
-  <div className="font-serif ">
-    <h3 className="text-sm mt-2 font-semibold">{title}</h3>
-    <div className="flex justify-between text-sm">
-      <p className="italic">
-        {company}, {location}
-      </p>
-      <p className=" text-gray-400">({date})</p>
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePoints = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="font-serif">
+      <div className="flex justify-between items-center mt-4 cursor-pointer" onClick={togglePoints}>
+        <div>
+          <h3 className="text-sm font-semibold">{title}</h3>
+          <div className="flex justify-between text-sm">
+            <p className="italic">
+              {company}, {location}
+            </p>
+            <p className="text-gray-400 ml-4">({date})</p>
+          </div>
+        </div>
+        <div
+          className={`transform transition-transform duration-300 ${
+            isOpen ? 'rotate-180' : 'rotate-0'
+          }`}
+        >
+          ▼
+        </div>
+      </div>
+      <div
+        className={`transition-max-height duration-1000 ease-in-out overflow-hidden ${
+          isOpen ? 'max-h-96' : 'max-h-0'
+        }`}
+      >
+        <ul className="list-disc p-4">
+          {points.map((point, index) => (
+            <li key={index} className="text-gray-600">
+              {point}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <ul className="list-disc p-4">
-      {points.map((point, index) => (
-        <li key={index} className="text-gray-600">
-          {point}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  );
+};
+
 const Experience: React.FC = () => {
   return (
     <div className="mt-12">
       <h2 className="text-xl font-bold">EXPERIENCE</h2>
 
+
       <ExperienceItem
-        title="Frontend Developer"
+        title="SDE"
         company="FuelBuddy"
         location="Gurugram, India"
-        date="Aug 2023 - Present"
+        date="June 2024 - Present"
+        points={[
+          "Shifted the customer application from Vue 3 to React Native, improving cross-platform compatibility and user experience.",
+          "Led the development of a dynamic reporting dashboard using React and D3.js, providing real-time data visualization and insights for better decision-making.",
+        ]}
+      />
+
+      <ExperienceItem
+        title="Frontend Developer Intern"
+        company="FuelBuddy"
+        location="Gurugram, India"
+        date="Aug 2023 - June 2024"
         points={[
           "Implemented a streamlined Payment-on-delivery (POD) system for drivers, optimizing transaction processes for improved operational efficiency.",
           "Enhanced the customer application with wallet transaction capabilities via Easebuzz, increasing payment convenience and user satisfaction by 2%.",
